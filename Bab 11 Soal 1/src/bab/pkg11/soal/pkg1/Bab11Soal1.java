db0a39d749d24fd2b6a809e670e3dd1443165058
Khalid3628 <144987495+Khalid3628@users.noreply.github.com>
2024-08-29 13:56:39 +0700

    Add files via upload

---
+++ Bab 11 Soal 1/src/bab/pkg11/soal/pkg1/Bab11Soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab.pkg11.soal.pkg1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Bab11Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Karyawan objek1=new Karyawan(1111, "Agus", "X", "Karyawan", 3000000);
+        Karyawan objek2=new Karyawan(1112,"Suharto", "Y", "CEO", 10000000);
+        objek1.info();
+        objek2.info();
+    }
+    
+}
